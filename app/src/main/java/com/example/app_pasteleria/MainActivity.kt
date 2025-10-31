ba8e2657b1ebf02d143a39d82e197de8cac6d7ea
Melanie <me.leivaf@duocuc.cl>
2025-10-31 01:28:56 -0300

    se agregan dependencias

--- app/src/main/java/com/example/app_pasteleria/MainActivity.kt
+++ app/src/main/java/com/example/app_pasteleria/MainActivity.kt
@@ -16,16 +16,9 @@ import com.example.app_pasteleria.ui.theme.AppPasteleriaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            AppPasteleriaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "la wea mala",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNav()
         }
     }
 }
--- app/src/main/java/com/example/app_pasteleria/viewmodel/CatalogoViewModel.kt
+++ app/src/main/java/com/example/app_pasteleria/viewmodel/CatalogoViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.app_pasteleria.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.app_pasteleria.data.model.Catalogo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ProductoViewModel : ViewModel(){
+    private val _pasteles = MutableStateFlow<List<Catalogo>>(emptyList())
+    val pasteles: StateFlow<List<Catalogo>> = _pasteles.asStateFlow()
+
+    fun guardarProducto(producto: Catalogo){
+        viewModelScope.launch {
+            // guardar en memoria
+
+            val nuevaLista = _pasteles.value + producto
+            _pasteles.value = nuevaLista
+        }
+
+    } // fin guardarProducto
+} // fin class
