041287f877320ab692843f7ec8fe1eab1c48f5d2
Prii231 <nicosanchezf@gmail.com>
2025-11-02 20:54:24 -0300

    Se realiza validaciones, felices50, correo duoc. Completas. Se termina el Push - Merge Completamente por discordancias.

--- app/src/main/java/com/example/app_pasteleria/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/app_pasteleria/data/repository/AuthRepository.kt
@@ -5,14 +5,11 @@ package com.example.app_pasteleria.data.repository
 import com.example.app_pasteleria.data.model.Credential
 
 class AuthRepository (
-<<<<<<< HEAD
-    private val validCredential: Credential = Credential.Admin
 
-=======
     private val validCredential: Credential = Credential.Admin,
     private val validCredential2: Credential = Credential.Usuario1,
     private val validCredential3: Credential = Credential.Usuario2
->>>>>>> 26de5656bdf7541c96f01dd1da90ca932097fd98
+
 ){
     fun login (correo:String,password:String): Boolean {
         //validad las credenciales que traemos desde el modelo
