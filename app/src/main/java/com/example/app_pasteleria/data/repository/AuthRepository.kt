c9bb92fbf9c218c5c5d13a8351ff7dffa39e32d6
Melanie <me.leivaf@duocuc.cl>
2025-11-02 19:00:27 -0300

    Creacion de packages y files

--- app/src/main/java/com/example/app_pasteleria/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/app_pasteleria/data/repository/AuthRepository.kt
@@ -6,11 +6,25 @@ import com.example.app_pasteleria.data.model.Credential
 
 class AuthRepository (
     private val validCredential: Credential = Credential.Admin
+
 ){
     fun login (correo:String,password:String): Boolean {
         //validad las credenciales que traemos desde el modelo
         return correo == validCredential.correo && password == validCredential.password
     }
+
+    fun Registro (correo: String, password: String): Boolean {
+        if (correo.isBlank() || password.isBlank()) {
+            return false
+        }
+        return true
+
+    }
+
+
+
+
+
 
 }//fin del fun
 
---
+++ app/src/main/java/com/example/app_pasteleria/ui/registro/RegistroUiState.kt
@@ -0,0 +1,7 @@
+package com.example.app_pasteleria.ui.registro
+
+data class RegistroUiState(
+    val correo : String = "",
+    val password : String = "",
+    val confirmarContraseña : String = ""
+)
